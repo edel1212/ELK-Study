7b810cc725b0e1674af7d18b74fadfa3999a207c
yoo <edel1212@naver.com>
2026-03-02 22:18:51 +0900

    logging server init

---
+++ logging-server/src/main/java/com/yoo/logging_server/LoggingServerApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.logging_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingServerApplication.class, args);
+	}
+
+}
---
+++ logging-server/src/test/java/com/yoo/logging_server/LoggingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.logging_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
